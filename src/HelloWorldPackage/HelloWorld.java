bf66fca8c29ed007ff7685ad1fd741085a91c510
Jon Lim <jonmickel.lim@pcc.edu>
2017-10-21 00:30:30 -0700

    Added start date of project.

--- src/HelloWorldPackage/HelloWorld.java
+++ src/HelloWorldPackage/HelloWorld.java
@@ -7,5 +7,6 @@ package HelloWorldPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to the project!");
+        System.out.println("This was created on 10/17/2017.");
     }
 }
